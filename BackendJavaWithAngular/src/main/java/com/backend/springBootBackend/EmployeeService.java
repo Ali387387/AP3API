565d20ba29bc9ffad024bdec7066277365eda362
Ali387387 <rajaali387387@gmail.com>
2021-12-15 16:58:21 +0500

    first

---
+++ BackendJavaWithAngular/src/main/java/com/backend/springBootBackend/EmployeeService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backend.springBootBackend;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Zorays
+ */
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository repo; 
+    public Employee registerEmployee(Employee emp)
+    {
+        return repo.save(emp);
+    }
+    
+    public List<Employee> getEmployee(){
+        return (List<Employee>) repo.findAll();
+        
+    }
+}
